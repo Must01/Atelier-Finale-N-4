9741b50dc03815917cfca78a47f7934a85582733
abdelbasset <abdelbassetlambarki@gmail.com>
2026-02-10 10:56:57 +0100

    Identity Section (Nom, Email, Level) with validation+Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CandidateFormComponent } from './components/candidate-form/candidate-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CandidateFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
